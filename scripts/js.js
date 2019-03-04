01846b31b42998fd57b39d856d520043ff87f16a
Andrey Alisenak <aalisenak@gmail.com>
2019-03-04 17:51:50 +0300

    git init

--- scripts/js.js
+++ scripts/js.js
@@ -1,5 +1,15 @@
 window.addEventListener('DOMContentLoaded', function () {
     'use strict';
+
+    document.body.onload = function () {
+        setTimeout(function () {
+            let preloader = document.querySelector('.preloader');
+            if (!preloader.classList.contains('none')) {
+                preloader.classList.add('none');
+            }
+        }, 1000);
+    };
+
     let burst = document.querySelectorAll('.burst');
 
     burst.forEach(function (item) {
