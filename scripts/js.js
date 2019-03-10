28119608dc36d8172c80b4c6112655afa3f4cc2c
Andrey Alisenak <aalisenak@gmail.com>
2019-03-10 21:50:55 +0300

    git init

--- scripts/js.js
+++ scripts/js.js
@@ -1,43 +1,43 @@
-window.addEventListener("DOMContentLoaded", function() {
-  "use strict";
+window.addEventListener("DOMContentLoaded", function () {
+    "use strict";
 
-  // PRELOADER
+    // PRELOADER
 
-  document.body.onload = function() {
-    setTimeout(function() {
-      let preloader = document.querySelector(".preloader");
-      if (!preloader.classList.contains("none")) {
-        preloader.classList.add("none");
-      }
-    }, 1000);
-  };
+    document.body.onload = function () {
+        setTimeout(function () {
+            let preloader = document.querySelector(".preloader");
+            if (!preloader.classList.contains("none")) {
+                preloader.classList.add("none");
+            }
+        }, 1000);
+    };
 
-  // BURST
+    // BURST
 
-  let burst = document.querySelectorAll(".burst");
+    let burst = document.querySelectorAll(".burst");
 
-  burst.forEach(function(item) {
-    item.addEventListener("mouseover", function() {
-      this.classList.add("animated", "rubberBand");
+    burst.forEach(function (item) {
+        item.addEventListener("mouseover", function () {
+            this.classList.add("animated", "rubberBand");
+        });
+        item.addEventListener("mouseout", function () {
+            this.classList.remove("animated", "rubberBand");
+        });
     });
-    item.addEventListener("mouseout", function() {
-      this.classList.remove("animated", "rubberBand");
+
+    // NAVIGATION
+
+    let burger = document.querySelector(".burger");
+    let menu = document.querySelector(".menu");
+    burger.addEventListener("click", function () {
+        burger.classList.toggle("active");
+
+        if (menu.classList.contains("close")) {
+            menu.classList.add("open", "animated", "pulse");
+            menu.classList.remove("close");
+        } else {
+            menu.classList.remove("open", "animated", "pulse");
+            menu.classList.add("close");
+        }
     });
-  });
-
-  // NAVIGATION
-
-  let burger = document.querySelector(".burger");
-  let menu = document.querySelector(".menu");
-  burger.addEventListener("click", function() {
-    burger.classList.toggle("active");
-
-    if (menu.classList.contains("close")) {
-      menu.classList.add("open", "animated", "pulse");
-      menu.classList.remove("close");
-    } else {
-      menu.classList.remove("open", "animated", "pulse");
-      menu.classList.add("close");
-    }
-  });
 });
